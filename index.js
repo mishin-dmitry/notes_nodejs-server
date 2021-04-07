02f583d398fa27138525b81f427889043d61060b
Mishin Dmitry <dmishin.dev@gmail.com>
2021-04-07 11:26:01 +0300

    made index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -9,3 +10,18 @@ nunjucks.configure("views", {
 });
 
 app.set("view engine", "njk");
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// Handle uncaught errors
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message);
+});
+
+const port = process.env.PORT || 300;
+
+app.listen(port, () => {
+  console.log(`  Listening on http://localhost:${port}`);
+});
